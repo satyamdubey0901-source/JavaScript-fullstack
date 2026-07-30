edbd9b9db7e48721494e1983cdda5700891a6516
satyamdubey0901-source <satyamdubey0901@gmail.com>
2026-07-30 23:18:48 +0530

    DataTypes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="satyam@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountId=2; it can not be changed 
+
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountCity="PratapGarh"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState])
+
+/*
+Prefer not to use var because of issue of functional and block scope
+*/
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all the js code as newer version
+
+//alert(3 +3 ) we are using node.js not browser
+
+console.log(3+3);
+
+console.log("Satyam")
+
+let name="Deepti"
+let age=123
+let isLoggedIn=false
+
+let state;
+
+console.log(typeof "Satyam");
+
+
+
