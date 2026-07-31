2cf7283a81ccdb7e7a3939c25f6229455125da85
satyamdubey0901-source <satyamdubey0901@gmail.com>
2026-07-31 09:44:57 +0530

    DataTypesSummary

---
+++ 01_basics/05_DatatypesSummary.js
@@ -0,0 +1,32 @@
+const score=100;
+const scoreValue=100.3
+
+const isLoggedIn = false;
+
+// Reference (Non-Primitive)
+// as Array,objects,functions
+
+const outsideTemp=null;
+let userEmail;// iski value undefined hogi
+
+// Declare symbols
+const id=Symbol ('123')
+const anotherId= Symbol('123')
+
+console.log(id===anotherId);
+
+// const bigNumber= 34565412345678901111n  yaha n lagate hi ye bigint ban jata hai
+
+// Arrays
+const heros=["shaktiman","nagraj","doga"]
+
+// objects declaration
+let myObj = {
+    name:"Satyam",
+    age:22,
+}
+
+// Function (ek variable ki tra bhi traet kar sakte ho)
+const myFunction=function(){
+    console.log("Hello world");
+}
