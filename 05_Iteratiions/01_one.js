37b9ad8106351791f6936e7073744801c35fb4b3
satyamdubey0901-source <satyamdubey0901@gmail.com>
2026-08-03 11:42:40 +0530

    Iteration

---
+++ 05_Iteratiions/01_one.js
@@ -0,0 +1,46 @@
+// for loop
+
+for (let i= 0; i < 10; i++) {
+    const element = i;
+    if(element == 5){
+        console.log("5 is best number")
+    }
+    console.log(element)
+}
+
+for(let i=1;i<=10;i++){ //  outer loop ki ek value ke karan innner loop 10 baar chalega
+    console.log(`Outer loop value : ${i}`);
+    for(let j=1;j<=10;j++){
+       // console.log(`Inner loop values : ${j} and inner loop ${i}`);
+       //console.log(i + '*' + j + '=' +  i*j)
+    }
+}
+
+
+// array par loop laga rhe hai
+let myArray=["flash","batman","superman"]
+console.log(myArray.length);
+for(let index = 0;index<myArray.length;index++){
+    const element = myArray[index];
+    console.log(element)
+}
+
+
+// break and continue
+for (let index = 1; index <= 20; index++) {
+    if(index==5){
+        console.log(`Detected 5`);
+        break;
+    }
+    console.log(`Value of index is ${index}`);
+    
+}
+
+// continue
+for (let i = 1; i <= 20; i++) {
+    if(i==5){
+        console.log(`we have got 5`);
+        continue; //isko use karte hi aap ssedhe loop se bahar jake 38 par pahuch jaoge with i=6
+    }
+    console.log(`Value of i is ${i}`);
+}
